9ddca72e4e2a85caf123785a2b421ef1a2b63b98
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-08-30 12:50:19 +0700

    Update margin dan boxicon

--- src/component/MainContent.svelte
+++ src/component/MainContent.svelte
@@ -4,31 +4,31 @@
     import Person from "../images/person.png"
 </script>
 
-<section class="px-[3em] py-[4.5em] text-white">
+<section class="max-w-6xl mx-auto py-[4.5em] text-white">
     <div class="text-center w-full gap-8 m-auto mb-8 text-dark-blue">
         <h6 class="font-semibold text-base">Our Team</h6>
-        <h4 class="text-[2rem] font-bold leading-10 my-2">Meet our team member</h4>
-        <p class="m-auto w-2/5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, quibusdam aliquam. Nulla!</p>
+        <h4 class="text-3xl font-bold leading-10 my-2">Meet our team member</h4>
+        <p class="m-auto max-w-md">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, quibusdam aliquam. Nulla!</p>
     </div>
-    <div class="flex justify-center items-center gap-4 text-center mx-20">
+    <div class="flex justify-center items-center gap-4 text-center max-w-5xl mx-auto">
         <div class="flex flex-wrap justify-center items-center gap-4 text-center">
             <img class="rounded-[3em]" src="{ContentMain}" alt="">
             <div class="text-dark-blue">
-                <h5 class="text-[2rem] font-bold leading-10 my-2">Derek Peterson</h5>
+                <h5 class="text-3xl font-bold leading-10 my-2">Derek Peterson</h5>
                 <p class="m-auto">Project Manager</p>
             </div>
         </div>
         <div class="flex flex-wrap justify-center items-center gap-4 text-center">
             <img class="rounded-[3em]" src="{ContentMain}" alt="">
             <div class="text-dark-blue">
-                <h5 class="text-[2rem] font-bold leading-10 my-2">Ashton Berube</h5>
+                <h5 class="text-3xl font-bold leading-10 my-2">Ashton Berube</h5>
                 <p class="m-auto">Marketing Consultant</p>
             </div>
         </div>
         <div class="flex flex-wrap justify-center items-center gap-4 text-center">
             <img class="rounded-[3em]" src="{ContentMain}" alt="">
             <div class="text-dark-blue">
-                <h5 class="text-[2rem] font-bold leading-10 my-2">Jimmy Forman</h5>
+                <h5 class="text-3xl font-bold leading-10 my-2">Jimmy Forman</h5>
                 <p class="m-auto">Project Manager</p>
             </div>
         </div>
@@ -41,7 +41,7 @@
         <h4 class="text-[2rem] font-bold leading-10 my-2">Which one is suitable for you</h4>
         <p class="m-auto w-2/5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda earum architecto, sunt at eos quo ex quidem delectus porro repellat.</p>
     </div>
-    <div class="flex mx-32">
+    <div class="flex max-w-5xl mx-auto mb-12">
         <div class="text-center flex flex-wrap mx-auto my-[2em] py-[2em] px-[1em] w-[20vw] gap-4">
             <div class="text-dark-blue flex flex-wrap justify-center m-auto items-center gap-2 w-[10vw] ">
                 <h5 class="text-lg font-semibold">Basic Plan</h5>
@@ -50,33 +50,23 @@
             <h4 class="text-[3rem] font-semibold mx-auto text-dark-blue">$30<span class="text-base font-semibold">/per month</span></h4>
             <ul class="text-dark-blue flex flex-wrap gap-4 w-[20vw] m-auto">
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 bg-light-blue border-2 rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-light-blue bg-white border-light-blue border-2 rounded-full"></button>    
                     30 GB Brandwidth    
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 bg-light-blue border-2 rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-light-blue bg-white border-light-blue border-2 rounded-full"></button>                
                     Business & finance analyzing   
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 bg-light-blue border-2 rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-light-blue bg-white border-light-blue border-2 rounded-full"></button>             
                     24 hour support
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 bg-light-blue border-2 rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-light-blue bg-white border-light-blue border-2 rounded-full"></button>               
                     Customer management 
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 bg-light-blue border-2 rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-light-blue bg-white border-light-blue border-2 rounded-full"></button>                
                     30 days version history   
                 </li>
             </ul>
@@ -89,33 +79,23 @@
             <h4 class="text-[3rem] font-semibold mx-auto text-white">$30<span class="text-base font-semibold">/per month</span></h4>
             <ul class="text-white flex flex-wrap gap-4 w-[20vw] mb-[0.8em]">
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="dark-blue" class="w-5 h-5 bg-white bg-[1em] rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-white bg-dark-blue border-white border-2 rounded-full"></button>               
                     30 GB Brandwidth    
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="dark-blue" class="w-5 h-5 bg-white bg-[1em] rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-white bg-dark-blue border-white border-2 rounded-full"></button>               
                     Business & finance analyzing   
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="dark-blue" class="w-5 h-5 bg-white bg-[1em] rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-white bg-dark-blue border-white border-2 rounded-full"></button>               
                     24 hour support
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="dark-blue" class="w-5 h-5 bg-white bg-[1em] rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-white bg-dark-blue border-white border-2 rounded-full"></button>                 
                     Customer management 
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="dark-blue" class="w-5 h-5 bg-white bg-[1em] rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-white bg-dark-blue border-white border-2 rounded-full"></button>                  
                     30 days version history   
                 </li>
             </ul>
@@ -131,39 +111,29 @@
             <h4 class="text-[3rem] font-semibold mx-auto text-dark-blue">$99<span class="text-base font-semibold">/per month</span></h4>
             <ul class="text-dark-blue flex flex-wrap gap-4 w-[20vw] m-auto">
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 bg-light-blue border-2 rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-light-blue bg-white border-light-blue border-2 rounded-full"></button>           
                     30 GB Brandwidth    
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 bg-light-blue border-2 rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-light-blue bg-white border-light-blue border-2 rounded-full"></button>       
                     Business & finance analyzing   
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 bg-light-blue border-2 rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-light-blue bg-white border-light-blue border-2 rounded-full"></button>               
                     24 hour support
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 bg-light-blue border-2 rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-light-blue bg-white border-light-blue border-2 rounded-full"></button>               
                     Customer management 
                 </li>
                 <li class="flex gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5 bg-light-blue border-2 rounded-full">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                    </svg>              
+                    <button disabled class="bx bx-check w-5 h-5 text-light-blue bg-white border-light-blue border-2 rounded-full"></button>               
                     30 days version history   
                 </li>
             </ul>
         </div>
     </div>
-    <div class="mx-[10em] flex gap-28">
+    <div class="max-w-5xl mx-auto flex gap-28">
         <div class="text-dark-blue w-full">
             <h6 class="font-semibold text-base">Testimonials</h6>
             <h4 class="text-[2rem] font-bold leading-10 my-4">Valuable feedback from our clients</h4>
--- src/component/MiddleContent.svelte
+++ src/component/MiddleContent.svelte
@@ -1,49 +1,27 @@
-<section class="bg-dark-blue bg-cover px-[3em] py-[4.5em] text-white">
-    <div class="flex flex-wrap items-center justify-center w-1/5 m-auto mb-8">
+<section class="bg-dark-blue bg-cover py-[4.5em] text-white">
+    <div class="flex flex-wrap items-center justify-center mb-8 max-w-[20vw] mx-auto">
         <h6 class="font-semibold text-base">Why Choose Us</h6>
         <h4 class="text-[2rem] font-bold leading-10 my-2">What we offer</h4>
     </div>
-    <div class="flex items-center justify-center gap-8 text-black cursor-pointer mx-20">
+    <div class="flex items-center justify-center gap-8 text-black cursor-pointer max-w-5xl mx-auto">
         <div class="bg-white bg-cover rounded-3xl px-6 py-6 pb-12">
             <div class="flex items-center justify-left gap-8 py-4">
-                <button>
-                    <svg 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    fill="none" 
-                    viewBox="0 0 24 24" 
-                    stroke-width="1.5" 
-                    stroke="currentColor" 
-                    class="h-12 p-[0.6rem] bg-oranye/[0.2] text-oranye rounded-full">
-                    <path stroke-linecap="round" 
-                        stroke-linejoin="round" 
-                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                    </svg>
-                </button>
-                <h5 class="text-[1.5rem] font-bold leading-10 my-2">Search Engine Optimization</h5>
+                <button class="bx bx-search-alt-2 h-14 w-20 p-4 text-2xl bg-oranye/[0.2] text-oranye rounded-full"></button>
+                <h5 class="text-2xl font-bold leading-10 my-2">Search Engine Optimization</h5>
             </div>
             <p class="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, dolorem perferendis maiores ut beatae consectetur!</p>
         </div>
         <div class="bg-white bg-cover rounded-3xl px-6 py-6 pb-12">
             <div class="flex items-center justify-left gap-8 py-4">
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-12 p-[0.6rem] bg-light-blue/[0.2] text-light-blue rounded-full">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
-                      </svg>
-                      
-                </button>
-                <h5 class="text-[1.5rem] font-bold leading-10 my-2">Social Media Development</h5>
+                <button class="bx bx-trending-up h-14 w-20 p-4 text-2xl bg-light-blue/[0.2] text-light-blue rounded-full"></button>
+                <h5 class="text-2xl font-bold leading-10 my-2">Social Media Development</h5>
             </div>
             <p class="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, dolorem perferendis maiores ut beatae consectetur!</p>
         </div>
         <div class="bg-white bg-cover rounded-3xl px-6 py-6 pb-12">
             <div class="flex items-center justify-left gap-8 py-4">
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-12 p-[0.6rem] bg-oranye/[0.2] text-oranye rounded-full">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12l-3-3m0 0l-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                      </svg>
-                      
-                </button>
-                <h5 class="text-[1.5rem] font-bold leading-10 my-2">Conversion Rate Optimization</h5>
+                <button class="bx bxs-file-doc h-14 w-20 p-4 text-2xl bg-oranye/[0.2] text-oranye rounded-full"></button>
+                <h5 class="text-2xl font-bold leading-10 my-2">Conversion Optimization</h5>
             </div>
             <p class="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, dolorem perferendis maiores ut beatae consectetur!</p>
         </div>
